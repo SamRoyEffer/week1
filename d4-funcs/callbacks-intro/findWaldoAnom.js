6ff3f6760b3b12ac2a902f529154d43d5013f0c6
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-28 21:11:38 -0400

    new files and folders

---
+++ d4-funcs/callbacks-intro/findWaldoAnom.js
@@ -0,0 +1,15 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function(names, found) {
+  names.forEach((name, index) => {
+    if (name === "Waldo") {
+      found(index);
+    }
+  })
+}
+
+const actionWhenFound = (index) => {
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+  console.log("Found Waldo at:", index);
+});
